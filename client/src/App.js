585d375ea10743143fdd144af30e95206b4a24f5
Adel Ahmed <adellhmt@gmail.com>
2024-05-19 12:54:46 +0300

    Add MongoDB connection file and yarn.lock

--- client/src/App.js
+++ client/src/App.js
@@ -45,12 +45,4 @@ function App() {
 
 export default App;
 
-mongoose.connect('mongodb://localhost:27017/mydatabase', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log('Connected to MongoDB');
-}).catch((error) => {
-  console.error('Error connecting to MongoDB:', error);
-});
 
--- client/src/index.js
+++ client/src/index.js
@@ -16,7 +16,4 @@ ReactDOM.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-const mongodbConnection = require('./mongodbConnection');
-
-mongodbConnection.run().catch(console.dir);
 
